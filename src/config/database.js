8f0e7768c506b7c4edbff933d87817817e2b8734
TIAGO AMERICANO RODRIGUES <tiago.americano.03@gmail.com>
2020-05-24 00:06:06 -0300

    database

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'Tiago2003',
-    database: 'cartola',
+    host: 'ruby.db.elephantsql.com',
+    username: 'guetvnee',
+    password: 'odatfP2IfqoRYG54iJq8JayTtwlSzoSz',
+    database: 'guetvnee',
     define: {
         timestamps: true,
         underscored: true,
     }
-}
+} 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = express()
 const routes = require('./routes')
 const cors = require('cors')
 
+
+
 require('./database')
 
 app.use(express.json())
